f92cbb835ef242645cf9353d83bb18a1d3377bfb
jiangjianqing <cz_jjq@163.com>
2015-07-28 17:14:27 +0800

    20150728下班前备份，增加了index.html中增加一些示例代码

--- js/controllers/phone.js
+++ js/controllers/phone.js
@@ -10,21 +10,52 @@
 define([
     'angular'
 ], function (angular) {
-    return ['$scope','$http','phoneStorage',
-        function ($scope,$http,phoneStorage) {
+    return ['$scope','$http','$log','$timeout','phoneStorage',
+        function ($scope,$http,$log,$timeout,phoneStorage) {
             $scope.phones = phoneStorage;
             $scope.showHello=function(){
-                alert('Hello！这是ng-click测试。');
+                //在controller中调用alert会出现$apply...的错误
+                //alert('Hello！这是ng-click测试。');
+                $log.info("showHello调用成功,这里使用alert会产生$apply already...的错误");
             };
             $scope.orderProp="age";
-
-            $scope.clock=new Date();
-            var updateClock=function(){
-                $scope.clock=new Date();
+            $scope.clock={
+                now:new Date()
             };
-            var timer=setInterval(function(){
-                $scope.$apply(updateClock);
-            },1000);
+
+            //$timeout与setTimeout作用相同，还可以不考虑闭包等问题
+            //$timeout(function(){
+            //    updateClock();
+            //},1000);
+
+            //timeout的使用方式
+            //var timeout;
+            //$scope.$watch('username', function(newUserName) {
+            //    if (newUserName) {
+            //        // 如果在进度中有一个超时(timeout)
+            //        if (timeout) $timeout.cancel(timeout);
+            //        timeout = $timeout(function() {
+            //            githubService.events(newUserName)
+            //                .success(function(data, status) {
+            //                    $scope.events = data.data;
+            //                });
+            //        }, 350);
+            //    }
+            //});
+            var timer;
+            var updateClock=(function func(){
+                $scope.clock.now=new Date();
+                //var tmp=arguments.callee;严格模式下无法使用
+                timer=$timeout(func,1000);
+            });
+            updateClock();
+            $scope.$on('$destroy',function(){
+                if(timer) $timeout.cancel(timer);
+            });
+            //var timer=setInterval(function(){
+            //   updateClock();
+                //$scope.$apply(updateClock);
+            //},1000);
 
             $http({
                 method: 'JSONP',
--- js/directives/nprLink.js
+++ js/directives/nprLink.js
@@ -13,11 +13,18 @@ define([
                 require: ['^ngModel'],
                 replace: true,
                 scope: {
-                    ngModel: '=',
-                    play: '&'
+                    ngModel: '=',// 将ngModel同指定对象绑定
+                    play: '&'    // 将引用传递给这个方法
+                    //,fromName: '@' // 储存与fromName相关联的字符串
                 },
                 templateUrl: 'js/views/nprListItem.html',
-                link: function(scope, ele, attr) {
+
+                /*
+                *如果没有设置require选项，那么controller参数的值为undefined。
+                *定义了require，才会有第四个controller参数，代表控制器或所依赖指令的控制器
+                * 如果require选项提供了一个指令数组，第四个参数会是一个由每个指令所对应的控制器组成的数组。
+                * */
+                link: function(scope, iElement,iAttrs,controller) {
                     scope.duration = scope.ngModel.audio[0].duration.$text;
                 }
             };
