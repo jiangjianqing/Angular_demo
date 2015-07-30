e7669b4f30b9520005f784cfe83d7cbf1c7cb994
jiangjianqing <cz_jjq@163.com>
2015-07-30 23:02:18 +0800

    20150730晚上备份,添加了.gitignore package.json，用npm管理依赖包

--- app/js/main.js
+++ app/js/main.js
@@ -7,7 +7,7 @@ require.config({
 		angular: '../../node_modules/angular/angular',
 		angularRoute: '../../node_modules/angular-route/angular-route',
 		angularMessages:'../../node_modules/angular-messages/angular-messages',
-		text: '../../node_modules/requirejs/text'
+		text: '../../node_modules/requirejs-text/text'
 	},
 	shim: {
 		angular: {
